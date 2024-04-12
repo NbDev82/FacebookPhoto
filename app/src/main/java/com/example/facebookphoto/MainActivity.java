d9d700b925dc448e0f128a3ddd7b38d0dd9e5a9a
Van Hoang <21110828@student.hcmute.edu.vn>
2024-04-12 14:10:58 +0700

    init view

--- app/src/main/java/com/example/facebookphoto/MainActivity.java
+++ app/src/main/java/com/example/facebookphoto/MainActivity.java
@@ -2,13 +2,46 @@ package com.example.facebookphoto;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.graphics.Bitmap;
 import android.os.Bundle;
+import android.widget.GridView;
+
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
+    private GridView mGridView;
+    private PhotoAdapter mPhotoAdapter;
+    private ArrayList<Bitmap> mPhotos;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Bitmap bitmap = Bitmap.createBitmap(200, 200, Bitmap.Config.ARGB_8888);
+
+
+        mGridView = findViewById(R.id.gridView);
+        mPhotos = new ArrayList<>();
+        mPhotos.add(bitmap);
+
+        mPhotoAdapter = new PhotoAdapter(this, mPhotos);
+        mGridView.setAdapter(mPhotoAdapter);
+
+//        Bitmap b = BitmapUtils.decodeSampledBitmapFromPath(
+//                "D:\\Android\\FacebookPhoto\\app\\src\\main\\res\\drawable\\ic_add.png",
+//                200,200
+//        );
+//        addPhoto(bitmap);
+    }
+
+    private void addPhoto(Bitmap photo) {
+//        mPhotos.add(photo);
+        mPhotoAdapter.addPhoto(photo);
+    }
+
+    private void deletePhoto(int position) {
+        mPhotos.remove(position);
+        mPhotoAdapter.notifyDataSetChanged();
     }
 }
